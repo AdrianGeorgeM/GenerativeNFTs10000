60393fd62ae1fd230b42393abbed19cb68d4bd9e
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-05-02 20:23:18 +0100

    Refactor createGif function for performance and efficiency

--- modules/HashlipsGiffer.js
+++ modules/HashlipsGiffer.js
@@ -1,26 +1,29 @@
 const GifEncoder = require('gif-encoder-2');
 const { writeFile } = require('fs/promises');
 
-function createGif(_canvas, _ctx, _fileName, _repeat, _quality, _delay) {
-	const gifEncoder = new GifEncoder(_canvas.width, _canvas.height);
-	gifEncoder.setQuality(_quality);
-	gifEncoder.setRepeat(_repeat);
-	gifEncoder.setDelay(_delay);
+function createGif({ width, height }, ctx, fileName, repeat, quality, delay) {
+	const gifEncoder = new GifEncoder(width, height);
+	gifEncoder.setQuality(quality);
+	gifEncoder.setRepeat(repeat);
+	gifEncoder.setDelay(delay);
+
+	const start = () => gifEncoder.start();
+	const add = () => gifEncoder.addFrame(ctx);
 
 	async function stop() {
 		try {
 			gifEncoder.finish();
 			const buffer = gifEncoder.out.getData();
-			await writeFile(_fileName, buffer);
-			console.log(`Created gif at ${_fileName}`);
+			await writeFile(fileName, buffer);
+			console.log(`Created gif at ${fileName}`);
 		} catch (error) {
 			console.error(`Error creating gif: ${error}`);
 		}
 	}
 
 	return {
-		start: () => gifEncoder.start(),
-		add: () => gifEncoder.addFrame(_ctx),
+		start,
+		add,
 		stop,
 	};
 }
