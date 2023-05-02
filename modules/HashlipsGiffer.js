ee0ebaffc5b8945efffdef07c0b7f39d2abd1894
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-05-02 20:11:47 +0100

    Refactored GifEncoder class to function for improved performance.

--- modules/HashlipsGiffer.js
+++ modules/HashlipsGiffer.js
@@ -1,38 +1,32 @@
-const GifEncoder = require("gif-encoder-2");
-const { writeFile } = require("fs");
+const GifEncoder = require('gif-encoder-2');
+const { writeFile } = require('fs');
 
-class HashLipsGiffer {
-  constructor(_canvas, _ctx, _fileName, _repeat, _quality, _delay) {
-    this.canvas = _canvas;
-    this.ctx = _ctx;
-    this.fileName = _fileName;
-    this.repeat = _repeat;
-    this.quality = _quality;
-    this.delay = _delay;
-    this.initGifEncoder();
-  }
+function createHashLipsGiffer(canvas, ctx, fileName, repeat, quality, delay) {
+	const gifEncoder = new GifEncoder(canvas.width, canvas.height);
+	gifEncoder.setQuality(quality);
+	gifEncoder.setRepeat(repeat);
+	gifEncoder.setDelay(delay);
 
-  initGifEncoder = () => {
-    this.gifEncoder = new GifEncoder(this.canvas.width, this.canvas.height);
-    this.gifEncoder.setQuality(this.quality);
-    this.gifEncoder.setRepeat(this.repeat);
-    this.gifEncoder.setDelay(this.delay);
-  };
+	const start = () => {
+		gifEncoder.start();
+	};
 
-  start = () => {
-    this.gifEncoder.start();
-  };
+	const add = () => {
+		gifEncoder.addFrame(ctx);
+	};
 
-  add = () => {
-    this.gifEncoder.addFrame(this.ctx);
-  };
+	const stop = () => {
+		gifEncoder.finish();
+		const buffer = gifEncoder.out.getData();
+		writeFile(fileName, buffer, (error) => {});
+		console.log(`Created gif at ${fileName}`);
+	};
 
-  stop = () => {
-    this.gifEncoder.finish();
-    const buffer = this.gifEncoder.out.getData();
-    writeFile(this.fileName, buffer, (error) => {});
-    console.log(`Created gif at ${this.fileName}`);
-  };
+	return {
+		start,
+		add,
+		stop,
+	};
 }
 
-module.exports = HashLipsGiffer;
+module.exports = createHashLipsGiffer;
